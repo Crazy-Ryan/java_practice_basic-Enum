80c56d0f969bc2598042b5a2cb6cc54d6cce98f4
Ryan.Wang <ranwang1995@outlook.com>
2020-02-17 01:27:26 +0800

    [WangRan] feat: finish main method in class Application

--- enum/src/main/java/Application.java
+++ enum/src/main/java/Application.java
@@ -1,7 +1,23 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Application {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        ArrayList<Student> studentArr = new ArrayList<>();
+        studentArr.add(new Student("Linda", Gender.FEMALE));
+        studentArr.add(new Student("Bob", Gender.MALE));
+        studentArr.add(new Student("Solider", Gender.MALE));
+        studentArr.add(new Student("Cindy", Gender.FEMALE));
 
-  }
+        for (Student student : studentArr) {
+            if (Gender.FEMALE == student.getGender()) {
+                System.out.println("======================");
+                System.out.println("Name: " + student.getName());
+                System.out.println("Name: " + student.getGender());
+            }
+        }
+
+    }
 
 }
